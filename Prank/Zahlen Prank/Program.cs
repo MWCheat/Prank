322d5618de9fedc98e2d43c989a68bbbeb928f14
MWCheat <147981268+MWCheat@users.noreply.github.com>
2023-12-14 16:39:07 +0100

    Add files via upload
    This is my first test it doesn't need to be good just fun for in a harmless maner

---
+++ Prank/Zahlen Prank/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Zahlen_Prank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geben sie eine ganze Zahl ein: ");
+            try
+            {
+                int zahlEingabe = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Convert.ToString(zahlEingabe));
+                Console.WriteLine("Vielen Dank");
+            }
+            catch
+            {
+                Console.WriteLine("You deserve this!");
+                Console.WriteLine("Have Fun!!!!!!!");
+
+                Console.Beep(100, 40);
+                Console.Beep(200, 40);
+                Console.Beep(300, 40);
+                Console.Beep(400, 40);
+                Console.Beep(500, 40);
+                Console.Beep(600, 40);
+                Console.Beep(700, 40);
+                Console.Beep(800, 40);
+                Console.Beep(900, 40);
+                Console.Beep(800, 40);
+                Console.Beep(700, 40);
+                Console.Beep(600, 40);
+                Console.Beep(500, 40);
+                Console.Beep(400, 40);
+                Console.Beep(300, 40);
+                Console.Beep(200, 40);
+                Console.Beep(100, 40);
+
+                Console.WriteLine("FUCK YOU");
+                Console.WriteLine("\t\t\t\t\t q");
+                Console.WriteLine("\t\t\t\t\tq q");
+                Console.WriteLine("\t\t\t\t\tq q");
+                Console.WriteLine("\t\t\t\t\tq q");
+                Console.WriteLine("\t\t\t\t\tq q");
+                Console.WriteLine("\t\t\t\t\tq q   qqq");
+                Console.WriteLine("\t\t\t\t\tq  q q\t q\t");
+                Console.WriteLine("\t\t\t\t     qq q   q\t  qq");
+                Console.WriteLine("\t\t\t\t    q  q\t   q");
+                Console.WriteLine("\t\t\t\t   q\t\t   q");
+                Console.WriteLine("\t\t\t\t   q\t\t   q");
+                Console.WriteLine("				   q		   q");
+                Console.WriteLine("\t\t\t\t  q\t\t   q");
+                Console.WriteLine("\t\t\t\t q\t\t   q");
+                Console.WriteLine("\t\t\t\t q\t\t   q");
+                Console.WriteLine("\t\t\t\t  q\t\t   q");
+                Console.WriteLine("\t\t\t\t    q\t\t   q");
+                Console.WriteLine("\t\t\t\t     q\t\t   q");
+                Console.WriteLine("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq\t\t   qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq");
+                Console.WriteLine("Press Enter to stop");
+                Console.ReadLine();
+
+
+                while (true)
+                {
+                    Console.Beep(100, 40);
+                    Console.Beep(200, 40);
+                    Console.Beep(300, 40);
+                    Console.Beep(400, 40);
+                    Console.Beep(500, 40);
+                    Console.Beep(600, 40);
+                    Console.Beep(700, 40);
+                    Console.Beep(800, 40);
+                    Console.Beep(900, 40);
+                    Console.Beep(800, 40);
+                    Console.Beep(700, 40);
+                    Console.Beep(600, 40);
+                    Console.Beep(500, 40);
+                    Console.Beep(400, 40);
+                    Console.Beep(300, 40);
+                    Console.Beep(200, 40);
+                    Console.Beep(100, 40);
+
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Prank/Zahlen Prank/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Zahlen Prank")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zahlen Prank")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("d6a89460-ef9c-44e7-8008-25fa50d9e25a")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
